99296cca125d4c9d0489be66473f6b70a6079a2d
CanIBeFii <filipe250502@gmail.com>
2023-01-31 15:25:37 +0000

    close mlx moved to key_events

--- sources/key_events.c
+++ sources/key_events.c
@@ -6,11 +6,12 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 11:42:11 by filipe            #+#    #+#             */
-/*   Updated: 2023/01/16 12:56:53 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/01/31 13:13:14 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include "mlx.h"
 
 int	key_press(int keycode, void *params)
 {
@@ -19,13 +20,35 @@ int	key_press(int keycode, void *params)
 	mlx = (t_window *)params;
 	if (keycode == ESC_KEY)
 		exit(close_mlx(mlx, SUCCESS));
-	else if (keycode == W_KEY || keycode == UP_KEY)
-		return (1);
-	else if (keycode == A_KEY || keycode == LEFT_KEY)
-		return (1);
-	else if (keycode == S_KEY || keycode == DOWN_KEY)
-		return (1);
-	else if (keycode == D_KEY || keycode == RIGHT_KEY)
-		return (1);
 	return (0);
+}
+
+int	close_mlx(t_window *mlx, int error_code)
+{
+	free_mlx(mlx);
+	return (error_code);
+}
+
+int	shutdown_mlx(t_window *mlx)
+{
+	exit(close_mlx(mlx, SUCCESS));
+}
+
+void	free_mlx(t_window *mlx)
+{
+	if (mlx != NULL)
+	{
+		if (mlx->data != NULL)
+		{
+			mlx_destroy_image(mlx->mlx, mlx->data->img);
+			free(mlx->data);
+		}
+		if (mlx->mlx_win != NULL)
+			mlx_destroy_window(mlx->mlx, mlx->mlx_win);
+		if (mlx->mlx != NULL)
+		{
+			mlx_destroy_display(mlx->mlx);
+			free(mlx->mlx);
+		}
+	}
 }
