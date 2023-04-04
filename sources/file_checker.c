da7306865608289724299c1d19df526dbecba115
CanIBeFii <filipe250502@gmail.com>
2023-04-04 13:57:37 +0100

    fix: small fix on error message

--- sources/file_checker.c
+++ sources/file_checker.c
@@ -6,7 +6,7 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 12:05:08 by filipe            #+#    #+#             */
-/*   Updated: 2023/01/31 11:19:27 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/04/04 12:44:06 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	file_checker(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		ft_printf("The number of arguments was %d instead of 2\n", &argc);
+		ft_printf("The number of arguments should be 1\n", &argc);
 		return (0);
 	}
 	if (check_filename(argv[1]) == 0)
